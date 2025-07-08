fd06f5f2e0eed0274b91615a0d4ba60cd552deb6
Natali Schers <schersnatali@gmail.com>
2025-07-08 16:12:28 -0300

    refactor: change parameter from name to ID

--- src/App.js
+++ src/App.js
@@ -3,34 +3,42 @@ import Banner from './components/Banner';
 import Form from './components/Form';
 import Team from './components/Team';
 import Footer from './components/Footer';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [teams, setTeams] = useState([
     {
+      id: uuidv4(),
       name: 'Programação',
       color: '#57C278'
     },
     {
+      id: uuidv4(),
       name: 'Front-End',
       color: '#82CFFA'
     },
     {
+      id: uuidv4(),
       name: 'Data Science',
       color: '#A6D157'
     },
     {
+      id: uuidv4(),
       name: 'Devops',
       color: '#E06B69'
     },
     {
+      id: uuidv4(),
       name: 'UX e Design',
       color: '#DB6EBF'
     },
     {
+      id: uuidv4(),
       name: 'Mobile',
       color: '#FFBA05'
     },
     {
+      id: uuidv4(),
       name: 'Inovação e Gestão',
       color: '#FF8A29'
     }
@@ -46,9 +54,9 @@ function App() {
     console.log('Deletando colaborador!');
   }
 
-  function changeTeamColor(newColor, teamName) {
+  function changeTeamColor(newColor, teamId) {
     setTeams(teams.map(team => {
-      if (team.name === teamName) {
+      if (team.id === teamId) {
         team.color = newColor;
       }
       return team;
@@ -64,6 +72,7 @@ function App() {
         <Team
           key={team.name}
           name={team.name}
+          id={team.id}
           primaryColor={team.color}
           employees={employees.filter(employee => employee.team === team.name)}
           onDelete={deleteEmployee}
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -6,7 +6,7 @@ const Team = (props) => {
     return (
         (props.employees.length > 0) &&
         <section className="team" style={{backgroundColor: hexToRgba(props.primaryColor, 0.6)}}>
-            <input onChange={e => props.onChangeTeamColor(e.target.value, props.name)} value={props.primaryColor} type="color" className="input-color" />
+            <input onChange={e => props.onChangeTeamColor(e.target.value, props.id)} value={props.primaryColor} type="color" className="input-color" />
             <h3>{props.name}</h3>
             <hr style={{borderColor: props.primaryColor}}/>
             <div className="employees">
